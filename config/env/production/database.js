9d973c748e0b7c919b92817fd28beb43b3f63df4
Anir <anirpoke@gmail.com>
2023-02-02 20:20:13 +0100

    database fix

---
+++ config/env/production/database.js
@@ -0,0 +1,12 @@
+// path: ./config/database.js
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+    },
+    useNullAsDefault: true,
+    debug: false,
+  },
+});
